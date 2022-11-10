e5cd1d8505ebc1de5a8e6f6f46999364b451d114
surya pragash <suryapragash.doodleblue@gmail.com>
2022-11-10 10:43:08 +0530

    mobile

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Mobile from "./components/nav";
+import Centre from "./components/centre";
+import Fooder from "./components/fooder";
+
+function App() {
+  return (
+    <div className="App">
+      <Mobile />
+      <Centre />
+      <Fooder />
+    </div>
+  );
+}
+
+export default App;
